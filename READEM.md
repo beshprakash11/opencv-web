5ae926cf4fa255091ef04b3414ecbd4cfb022bf2
beshprakash11 <beshprakashktm@gmail.com>
2023-03-14 12:31:39 +0100

    project reference upateD

--- READEM.md
+++ READEM.md
@@ -1,3 +1,6 @@
 # image processing web pages
 # Run app 
 - uvicorn main:app --reload
+
+# reference
+https://stackoverflow.com/questions/65971081/stream-video-to-web-browser-with-fastapi
--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def index():
 async def imgUpload(file:UploadFile = File(...)):
     return {'image' : 'sample image'}
 
+@app.get('/api/network/image')
+async def readImage():
+    return {'image' : '<img src0'}
+
 # defining host and port
 if __name__ == '__main__':
     uvicorn.run(app) # , host='0.0.0.0', port=8005)
