2bf40bcd659eeac0418e0236cf7046ba46af995b
beshprakash11 <beshprakashktm@gmail.com>
2023-03-14 12:11:42 +0100

    main file updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
-
+import uvicorn
 
 app = FastAPI()
 
@@ -10,3 +10,7 @@ def index():
 @app.post('/api/image')
 async def imgUpload(file:UploadFile = File(...)):
     return {'image' : 'sample image'}
+
+# defining host and port
+if __name__ == '__main__':
+    uvicorn.run(app) # , host='0.0.0.0', port=8005)
